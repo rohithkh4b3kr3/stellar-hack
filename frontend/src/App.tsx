84eedd6f00cb6ca85146e5701058cfe13ea1276e
Rohith vishva <rohithvishva2005@gmail.com>
2026-02-07 10:13:34 +0000

    initiaL

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function App(props: Props) {
+  const {} = props
+
+  return (
+    <>
+   <h1>frontend</h1> 
+    </>
+  )
+}
+
+export default App
